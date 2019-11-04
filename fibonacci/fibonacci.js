7bd067d14c4d040ce2f6c15b9e16d696625208a3
Curran <currandhaliwal@gmail.com>
2019-11-04 22:30:26 +0000

    completed fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [1,1];
+    let num = Number(n);
+    if (num <= 0){
+        return "OOPS";
+    }
+    for (i = 2; i <= num; i++){
+        arr.push((arr[i-1]+arr[i-2]));
+    }
+    return arr[num-1];
 }
 
 module.exports = fibonacci
