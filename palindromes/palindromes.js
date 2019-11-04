7eaa3a90a6db64c9e0cbd3294e016703cd6a1090
Curran <currandhaliwal@gmail.com>
2019-11-04 18:57:49 +0000

    Completed palindrome exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,11 +1,10 @@
 const palindromes = function(word) {
-    let wordArray = word.split("");
-    let revArr = [];
-    wordArray.repl
-    for (let i = wordArray.length - 1; i>=0; i--){
-        revArr.push(wordArray[i]);
-    }
-    return (word == revArr.join(""));
+    
+    let processedWord = word.toLowerCase().replace(/[\W]/g,"");
+    let reversedWord = processedWord.split("").reverse().join("");
+
+    return (processedWord == reversedWord);
+    
 }
 
 module.exports = palindromes
