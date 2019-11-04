5e42dbf3bd46faad4062356fb947251ef9ac8825
Curran <currandhaliwal@gmail.com>
2019-11-04 22:56:54 +0000

    findTheOldest Complete

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+   return people.reduce((oldest,current) => getAge(oldest) > getAge(current) ? oldest : current)
+}
 
+function getAge(person){
+  if (!person.yearOfDeath){
+    return (new Date().getFullYear())-person.yearOfBirth;
+  }
+  return person.yearOfDeath - person.yearOfBirth;
 }
 
 module.exports = findTheOldest
