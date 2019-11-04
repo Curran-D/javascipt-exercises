c972ffb039d7a7c183467eb7f3c1d5f659bed38d
Curran <currandhaliwal@gmail.com>
2019-11-04 18:35:11 +0000

    Completed test 1 of palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
-
+const palindromes = function(word) {
+    let wordArray = word.split("");
+    let revArr = [];
+    wordArray.repl
+    for (let i = wordArray.length - 1; i>=0; i--){
+        revArr.push(wordArray[i]);
+    }
+    return (word == revArr.join(""));
 }
 
 module.exports = palindromes
